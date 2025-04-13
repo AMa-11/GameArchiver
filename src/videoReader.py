0b3bf79ca1f24d96748fff27d066eb4250e4000e
Memo <137224267+AMa-11@users.noreply.github.com>
2025-04-13 21:23:31 +0200

    Add basic multiprocessing to VideoReader

--- src/videoReader.py
+++ src/videoReader.py
@@ -1,31 +1,99 @@
 import numpy as np
 import cv2 as cv
+import os
 import cProfile
 import math
 import time
+import multiprocessing as mp
 
-cap = cv.VideoCapture('videos/HortusdeEscapismo.mkv')
+# constants
+PROCESSES_COUNT = 3
+# should be 1~5
+SECONDS_INTERVAL = 200
+FILE_LOCATION = 'videos/HortusdeEscapismo.mkv'
+
+
+# get video
+cap = cv.VideoCapture(FILE_LOCATION)
+
+# extract relevant parameters
 frame_count = cap.get(cv.CAP_PROP_FRAME_COUNT)
 fps = cap.get(cv.CAP_PROP_FPS)
-# set offset by the amount of time/frames you want to skip
-# example:
-# want to skip 2 seconds
-frame_offset = 50 * fps
+frame_offset = SECONDS_INTERVAL * fps
+partitions = np.arange(start = 1,
+                        stop = frame_count,
+                        step = math.floor(frame_count / PROCESSES_COUNT),
+                        dtype=int
+)
+# create pairs of starting and ending frames
+pairs_start_end = []
+for i in range(partitions.size - 1):
+    pairs_start_end.append([partitions[i], partitions[i + 1]])
+if(partitions[-1] < frame_count):
+    pairs_start_end.append([partitions[-1], int(frame_count)])
+
 print("Video Total Frames:", frame_count)
 print("Video Frame Rate:", fps)
 print("Number of iterations:", math.floor(frame_count/frame_offset))
-
+print("Partition Step:", math.floor(frame_count / PROCESSES_COUNT))
+print("Partitions:", partitions)
+print("Start End Pairs:", pairs_start_end)
 
 frames = np.arange(start = 1, stop = frame_count, step = frame_offset, dtype=int)
 
-def processVideo():
+def processesStart():
+
+    # store Futures of the processes to wait for them to conclude
+    futures = []
+
+    # consider initializing with videoCapture as shared variables
+    pool = mp.Pool(processes = PROCESSES_COUNT)
+
+    # loop over pairs and start processes
+    for pair in pairs_start_end:
+        f = pool.apply_async(processVideoFrameRange, args=(pair[0], pair[1]))
+        futures.append(f)
+
+    for f in futures:
+        f.get()
+    
+    cap.release()
+    print("Processes Complete")
+
+# processes the frames in the range
+def processVideoFrameRange(startFrame, endFrame):
+    print("I am Process: ", os.getpid())
+    print("I am responsible for:[%d, %d]" % (startFrame, endFrame))
+
+    cap = cv.VideoCapture(FILE_LOCATION)
+
+    frames = np.arange(start = startFrame, stop = endFrame, step = frame_offset, dtype=int)
+    print("My frames:", frames)
+
     for frame in frames:
+        cap.set(cv.CAP_PROP_POS_FRAMES, frame)
+        ret, frame = cap.read()
+        #when using 3 processes encountering 
+        #!_src.empty() in function 'cv::cvtColor'
         
+        try:
+            gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        except Exception as e:
+            print("I am", os.getpid())
+            print("I am throwing an exception:", e)
+
+    cap.release()
+    print("Video Ended")
+
+
+
+def processVideoSingleProcess():
+    for frame in frames:
         t0 = time.perf_counter()
         cap.set(cv.CAP_PROP_POS_FRAMES, frame)
         t1 = time.perf_counter()
         dt = t1 - t0
-        print(f"jump to {frame:3d}: {dt:.3f} s", "*" * int(round(dt/0.1)))
+        #print(f"jump to {frame:3d}: {dt:.3f} s", "*" * int(round(dt/0.1)))
 
         # read the next frame
         # if frame is read correctly ret is True
@@ -38,36 +106,9 @@ def processVideo():
 
     cap.release()
     print("Video Ended")
-    cv.destroyAllWindows()
+    
 
-
-cProfile.run("processVideo()")
-
-# older code
-'''
-# loop over the frames
-def processVideo1():
-    frame_number = 0
-    while cap.isOpened():
-
-        # update frame number
-        frame_number += 1
-        # set target to next frame
-        cap.set(cv.CAP_PROP_POS_FRAMES, frame_number * frame_offset)
-
-        # read the next frame
-        # if frame is read correctly ret is True
-        ret, frame = cap.read()
-        if not ret:
-            print("Can't receive frame (stream end?). Exiting ...")
-            break
-
-        # operations on frame here
-        # couple into processFrame() function
-        # if it becomes more complex
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        
-    cap.release()
-    print("Video Ended")
-    cv.destroyAllWindows()
-'''
+if __name__ == '__main__':
+    processesStart()
+    #cProfile.run("processesStart()")
+    #processVideoSingleProcess()
